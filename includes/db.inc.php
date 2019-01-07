ec559c157636ea1bf192e69a159da3d3aa2cafc9
akshat0047 <pande.akshat21@gmail.com>
2019-01-07 22:10:43 +0530

    trying with xampp

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -3,12 +3,13 @@
 
 $database= "localhost";
 $databaseuser="root";
-$databasepwd="Akshat0047$";
+$databasepwd="akshat0047";
 $databasename="STORE";
 
 $conn= mysqli_connect($database,$databaseuser,$databasepwd,$databasename);
 if (!$conn) {
     die('Could not connect: ' . mysqli_error());
 }
+
 
  ?>
